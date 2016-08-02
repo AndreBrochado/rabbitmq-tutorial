21d984cb262825c20a818d232455cb2f60eb74d4
Andre <andrebrreis@gmail.com>
2016-08-02 11:42:53 +0100

    2nd tutorial step 1 done

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -7,9 +7,15 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class Receiver {
+public class Worker {
 
     private final static String QUEUE_NAME = "hello";
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
 
     public static void main(String[] argv)
             throws java.io.IOException,
@@ -23,12 +29,19 @@ public class Receiver {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        Consumer consumer = new DefaultConsumer(channel) {
+        final Consumer consumer = new DefaultConsumer(channel) {
             @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-                    throws IOException {
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
+
                 System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(" [x] Done");
+                }
             }
         };
         channel.basicConsume(QUEUE_NAME, true, consumer);
